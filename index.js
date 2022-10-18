cc185e8a9286a512ad5f5b8b0aeebfec5495de6f
Thomas <stephennelsonstne@gmail.com>
2022-10-18 15:26:19 -0500

    added nodeJS things

---
+++ index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+const require = require('inquirer');
+// TODO: Create an array of questions for user input
+const questions = [
+    { name:}
+
+
+
+
+
+
+
+
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
